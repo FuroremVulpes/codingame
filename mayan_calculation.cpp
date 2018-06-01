76f8d4a8427918dc44bdf5f9581d4c55686aacba
Tara Vulpes <martynyuk.darya@gmail.com>
2018-06-01 18:56:38 +0300

    dummy for the Mayan Calculation task

---
+++ mayan_calculation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int L;
+    int H;
+    cin >> L >> H; cin.ignore();
+    for (int i = 0; i < H; i++) {
+        string numeral;
+        cin >> numeral; cin.ignore();
+    }
+    int S1;
+    cin >> S1; cin.ignore();
+    for (int i = 0; i < S1; i++) {
+        string num1Line;
+        cin >> num1Line; cin.ignore();
+    }
+    int S2;
+    cin >> S2; cin.ignore();
+    for (int i = 0; i < S2; i++) {
+        string num2Line;
+        cin >> num2Line; cin.ignore();
+    }
+    string operation;
+    cin >> operation; cin.ignore();
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "result" << endl;
+}

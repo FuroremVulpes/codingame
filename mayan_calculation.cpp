f0ede4983f96fe741804fe2e45f8eed66157b59f
Tara Vulpes <martynyuk.darya@gmail.com>
2018-06-03 20:53:53 +0300

    Mayan to dec map

--- mayan_calculation.cpp
+++ mayan_calculation.cpp
@@ -9,14 +9,22 @@ using namespace std;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+ map <int, vector<string>> number_table;
+
+ void set_number_table(string[] numbers, int L, int H);
+
 int main()
 {
     int L;
     int H;
+    string numbers[H];
+    int result;
     cin >> L >> H; cin.ignore();
     for (int i = 0; i < H; i++) {
         string numeral;
         cin >> numeral; cin.ignore();
+        numbers[i] = numeral;
     }
     int S1;
     cin >> S1; cin.ignore();
@@ -37,4 +45,15 @@ int main()
     // To debug: cerr << "Debug messages..." << endl;
 
     cout << "result" << endl;
+}
+
+void set_number_table(string[] numbers, int L, int H) {
+    for (size_t  i = 0; i < 20; ++i) {
+        vector<string> one_number;
+        for (size_t j = 0; j < H; ++j) {
+            one_number.push_back(numbers[j].substr(i*L, L));
+        }
+        number_table.insert(pair<int, vector<string>>(i, one_number));
+    }
+  }
 }
